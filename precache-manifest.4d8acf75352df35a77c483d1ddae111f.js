c190f02662db8d28fb60cbceab895e12cc583240
unknown <13by13@queensu.ca>
2019-10-30 19:15:36 -0400

    Updates

---
+++ precache-manifest.4d8acf75352df35a77c483d1ddae111f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9668bb3571558a5b3b62910d1d6f0cf",
+    "url": "/profilesite/index.html"
+  },
+  {
+    "revision": "a9f71140427c0707e7bc",
+    "url": "/profilesite/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d954d15d07f3944f56f0",
+    "url": "/profilesite/static/js/2.d58f72bd.chunk.js"
+  },
+  {
+    "revision": "a9f71140427c0707e7bc",
+    "url": "/profilesite/static/js/main.23028385.chunk.js"
+  },
+  {
+    "revision": "29494a7cad4c4efacd22",
+    "url": "/profilesite/static/js/runtime-main.3831e9df.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/profilesite/static/media/logo.25bf045c.svg"
+  }
+]);
